a3c523d6fbc28c6983ad9e67554ae73cab994ed9
tsorrel3 <tsorrel3@jhu.edu>
2016-09-14 13:43:14 -0400

    added design document

---
+++ File_Transfer_Application/packet_header.h
@@ -0,0 +1,11 @@
+
+
+typedef struct header {
+    int seq_num;
+    int type;
+    int size;
+} packet_header;
+
+
+
+#define WINDOW_SIZE 10
--- File_Transfer_Application/rcv.c
+++ File_Transfer_Application/rcv.c
@@ -10,13 +10,77 @@
 
 
 #include "net_include.h"
+#include "packet_header.h"
 
+
+#define NAME_LENGTH 80
 
 int main (int argc, char** argv)
-{
-  int lossRate = atoi(argv[1]);
+{    
+    /* socket declarations */
+
+    struct sockaddr_in    name;    
+    struct sockaddr_in    send_addr;
+    struct sockaddr_in    from_addr;
+    socklen_t             from_len;
+    struct hostent        h_ent;
+    struct hostent        *p_h_ent;
+    char                  host_name[NAME_LENGTH] = {'\0'};
+    char                  my_name[NAME_LENGTH] = {'\0'};
+    int                   host_num;
+    int                   from_ip;
+    int                   ss,sr;
+    fd_set                mask;
+    fd_set                dummy_mask,temp_mask;
+    int                   bytes;
+    int                   num;
+    char                  mess_buf[MAX_MESS_LEN];
+    char                  input_buf[80];
+    struct timeval        timeout;
+
+    int lossRate = atoi(argv[1]);
+
+    /* set up receive socket */
+    sr = socket(AF_INET, SOCK_DGRAM, 0);  /* socket for receiving (udp) */
+    if (sr<0) {
+	perror("Ucast: socket");
+	exit(1);
+    }
+
+    name.sin_family = AF_INET; 
+    name.sin_addr.s_addr = INADDR_ANY; 
+    name.sin_port = htons(PORT);
 
 
+    if ( bind( sr, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
+	perror("Ucast: bind");
+	exit(1);
+    }
 
-  return 0;
+ 
+    send_addr.sin_family = AF_INET;
+    send_addr.sin_addr.s_addr = host_num; 
+    send_addr.sin_port = htons(PORT);
+
+    
+    FD_ZERO( &mask );
+    FD_ZERO( &dummy_mask );
+    FD_SET( sr, &mask );
+
+    /* event loop */   
+    for(;;)
+    {
+	
+    }
+
+
+    /* set up sending socket */ 
+    ss = socket(AF_INET, SOCK_DGRAM, 0); /* socket for sending (udp) */
+    if (ss<0) {
+	perror("Ucast: socket");
+	exit(1);
+    }
+    
+
+    return 0;
 }
