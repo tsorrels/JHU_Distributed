fb6331b23cbd3872cc631c05b0670855d2521aff
tsorrel3 <tsorrel3@jhu.edu>
2016-11-16 18:47:08 -0500

    initial commit for Mail delivery system

---
+++ Distributed_Mail_System/client.c
@@ -0,0 +1,28 @@
+#include "sp.h"
+#include "mail.h"
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+
+
+int main (int argc, char ** argv)
+{
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
