13f5810188681b6159ca62cee6c30aa9c0c33436
tsorrel3 <tsorrel3@jhu.edu>
2016-12-02 08:29:44 -0500

    built recover structure

--- Distributed_Mail_System/recover.c
+++ Distributed_Mail_System/recover.c
@@ -13,8 +13,31 @@
 // state needs fd for recovery contents file
 
 
+static void recover(int fd){
+    int numBytesRead;
+    char recoveryBuffer[sizeof(recovery_meta)];
+    recovery_meta * recoveryStruct;
+
+    numBytesRead = read(fd, recoveryBuffer, sizeof(recovery_meta));
+    if (numBytesRead != sizeof(recovery_meta)){
+	perror("In recover, failed to read entire recover_meta struct");
+    }
+
+    recoveryStruct = (recovery_meta *) recoveryBuffer;
+
+    if (recoveryStruct->writing != 0){
+	perror("In recover, loaded non-zero value for writing");
+    }
+
+
+}
+
+
 void loadState(state * local_state){
     struct stat st;
+    int stateExists;
+    stateExists = 1;
+
 
     if (stat("./recovery", &st) == -1) {
         mkdir("./recovery", 0700);
@@ -24,8 +47,15 @@ void loadState(state * local_state){
 
     // set fd in state
 
-    local_state->recoveryFD = open(CONTENTS, O_CREAT);
+    if (stat(CONTENTS, &st) == -1) {
+        stateExists = 0;
+    }    
     
+    local_state->recoveryFD = open(CONTENTS, O_CREAT);
+
+    if (stateExists == 1){
+	recover(local_state->recoveryFD);
+    }
 
 }
 
